11a777f8252bfdd02a65a77746a1e64270658db7
Nikita <lavro.nikita123@gmail.com>
2023-02-14 12:57:04 +0200

    userService registration, checking existence, adding new user and hashing pass

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,3 +1,17 @@
-class UserService {}
+const UserModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+
+class UserService {
+  async registration(email, password) {
+    const candidateExists = await UserModel.findOne({ email });
+
+    if (candidateExists) {
+      throw new Error(`Candidate with email of ${email} already exists`);
+    }
+
+    const hashPassword = await bcrypt.hash(password, 3);
+    const user = await UserModel.create({ email, hashPassword });
+  }
+}
 
 module.exports = new UserService();
