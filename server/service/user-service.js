cd6e5b8fb35d7cc2bffe5a27a7e7dea593cbc964
Nikita <lavro.nikita123@gmail.com>
2023-02-14 13:27:14 +0200

    saving tokens login and return

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcrypt");
 const uuid = require("uuid");
 const mailService = require("./mail-service");
 const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
 
 class UserService {
   async registration(email, password) {
@@ -14,13 +15,22 @@ class UserService {
 
     const hashPassword = await bcrypt.hash(password, 3);
     const activationLink = uuid.v4();
+
     const user = await UserModel.create({
       email,
       hashPassword,
       activationLink,
     });
     await mailService.sendActivationMail(email, activationLink);
-    const tokens = tokenService.generateTokens();
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return {
+      ...tokens,
+      user: userDto,
+    };
   }
 }
 
