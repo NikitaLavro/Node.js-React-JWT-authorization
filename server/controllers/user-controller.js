e469e15608693d7f7d84e6b9cebe148c971e0cd6
Nikita <lavro.nikita123@gmail.com>
2023-02-16 01:16:07 +0200

    UserController registration error implementation

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,8 +1,15 @@
 const userService = require("../service/user-service");
+const { validationResult } = require("express-validator");
+const ApiError = require("../exceptions/api-error.js");
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest("Validation Error", errors.array()));
+      }
+
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
 
