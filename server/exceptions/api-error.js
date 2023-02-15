b7aecd7364f06a2e6aa305bcaa088285c0644c0e
Nikita <lavro.nikita123@gmail.com>
2023-02-16 00:40:34 +0200

    Api_Error initial class structure

---
+++ server/exceptions/api-error.js
@@ -0,0 +1,10 @@
+module.export = class ApiError extends Error {
+  status;
+  errors;
+
+  constructor(status, message, errors) {
+    super(message);
+    this.status = status;
+    this.errors = errors;
+  }
+};
