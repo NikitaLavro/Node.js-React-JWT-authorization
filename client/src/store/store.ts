20b9478e1f82348fbacb20accf4bc779c89ba678
Nikita <lavro.nikita123@gmail.com>
2023-02-17 21:55:55 +0200

    store structure and login method

---
+++ client/src/store/store.ts
@@ -0,0 +1,31 @@
+import { makeAutoObservable } from "mobx";
+import { IUser } from "../models/response/IUser";
+import AuthService from "../services/AuthService";
+
+export default class Store {
+  user = {} as IUser;
+  isAuth = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  setAuth(bool: boolean) {
+    this.isAuth = bool;
+  }
+
+  setUser(user: IUser) {
+    this.user = user;
+  }
+
+  async login(email: string, password: string) {
+    try {
+      const response = await AuthService.login(email, password);
+      localStorage.setItem("token", response.data.accessToken);
+      this.setAuth(true);
+      this.setUser(response.data.user);
+    } catch (error: any) {
+      console.log(error.response?.data?.message);
+    }
+  }
+}
