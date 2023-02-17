0b5a9456216fb40f7b24120eddd7a96bdc6055db
Nikita <lavro.nikita123@gmail.com>
2023-02-17 21:17:01 +0200

    AuthService registration and logout functions

--- client/services/AuthService.ts
+++ client/services/AuthService.ts
@@ -9,4 +9,15 @@ export default class AuthService {
   ): Promise<AxiosResponse<AuthResponse>> {
     return $api.post<AuthResponse>("/login", { email, password });
   }
+
+  static async registration(
+    email: string,
+    password: string
+  ): Promise<AxiosResponse<AuthResponse>> {
+    return $api.post<AuthResponse>("/registration", { email, password });
+  }
+
+  static async logout(): Promise<void> {
+    return $api.post("/logout");
+  }
 }
