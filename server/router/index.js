49e428b4fde980b1b32499a59aad9e5076d6aa42
Nikita <lavro.nikita123@gmail.com>
2023-02-17 22:20:04 +0200

    server authMiddleware import fix

--- server/router/index.js
+++ server/router/index.js
@@ -2,7 +2,7 @@ const Router = require("express").Router;
 const userController = require("../controllers/user-controller");
 const router = new Router();
 const { body } = require("express-validator");
-const authMiddleware = require("./middlewares/auth-middleware");
+const authMiddleware = require("../middlewares/auth-middleware");
 
 router.post(
   "/registration",
