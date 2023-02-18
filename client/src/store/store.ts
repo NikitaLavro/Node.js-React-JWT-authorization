856acf9c13a015f83e924d30f16ef649e811043c
Nikita <lavro.nikita123@gmail.com>
2023-02-18 12:17:55 +0200

    Store isLoading implementation

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -8,6 +8,7 @@ import AuthService from "../services/AuthService";
 export default class Store {
   user = {} as IUser;
   isAuth = false;
+  isLoading = false;
 
   constructor() {
     makeAutoObservable(this);
@@ -19,6 +20,10 @@ export default class Store {
 
   setUser(user: IUser) {
     this.user = user;
+  }
+
+  setLoading(bool: boolean) {
+    this.isLoading = bool;
   }
 
   async login(email: string, password: string) {
@@ -55,6 +60,7 @@ export default class Store {
   }
 
   async checkAuth() {
+    this.setLoading(true);
     try {
       const response = await axios.get<AuthResponse>(`${API_URL}/refresh`);
       localStorage.removeItem("token");
@@ -62,6 +68,8 @@ export default class Store {
       this.setUser(response.data.user);
     } catch (error: any) {
       console.log(error.response?.data?.message);
+    } finally {
+      this.setLoading(false);
     }
   }
 }
