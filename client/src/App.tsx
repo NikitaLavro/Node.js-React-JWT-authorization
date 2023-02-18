c6d0eb11e0db05fd9bac2d795d7382a454acf6a9
Nikita <lavro.nikita123@gmail.com>
2023-02-18 12:07:29 +0200

    App useEffect and component user email dynamic render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Context } from ".";
 import LoginForm from "./components/LoginForm";
+import { observer } from "mobx-react-lite";
 
 function App() {
+  const { store } = useContext(Context);
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      store.checkAuth();
+    }
+  }, []);
+
   return (
     <div className="App">
+      <h1>
+        {store.isAuth
+          ? `User is logged in ${store.user.email}`
+          : `Please log in`}
+      </h1>
       <LoginForm />
     </div>
   );
 }
 
-export default App;
+export default observer(App);
